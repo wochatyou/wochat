a7ec131bae83e5930014925cbb8ca3d0dceb9997
My Name <my-name@chromium.org>
2024-01-23 18:16:56 -0800

    Init commit

---
+++ win/pch.h
@@ -0,0 +1,13 @@
+#ifndef __WT_PCH_H__
+#define __WT_PCH_H__
+
+
+#include <atlbase.h>
+#include <atlwin.h>
+#include <d2d1.h>
+#include <dwrite.h>
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
+
+
+#endif // __WT_PCH_H__
---
+++ win/resource.h
@@ -0,0 +1,3 @@
+#define IDR_MAINFRAME           128
+#define IDR_DEFAULTFONT			129
+#define IDR_ASCIIFONT           130
